3c7bd744752bca0538b4ca8463adac48dbb9729f
ZongGenXu <540682674@qq.com>
2017-10-29 14:52:00 +0800

    ...

--- ZGPhotoBrowser/ZGPhotoBrowser/ViewController.m
+++ ZGPhotoBrowser/ZGPhotoBrowser/ViewController.m
@@ -7,8 +7,31 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+WebCache.h"
 
-@interface ViewController ()
+@interface ZGTestCell : UICollectionViewCell
+@property (nonatomic, strong) UIImageView *imgView;
+@end
+
+@implementation ZGTestCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        _imgView = [[UIImageView alloc] initWithFrame:self.contentView.bounds];
+        [self.contentView addSubview:_imgView];
+    }
+    return self;
+}
+
+@end
+
+
+
+@interface ViewController () <UICollectionViewDataSource,UICollectionViewDelegate>
+
+@property (nonatomic, strong) UICollectionView *collectionView;
 
 @end
 
@@ -16,14 +39,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    [self setupViews];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupViews
+{
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.itemSize = CGSizeMake(100, 100);
+    _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    [_collectionView registerClass:[ZGTestCell class] forCellWithReuseIdentifier:@"testCell"];
+    [self.view addSubview:_collectionView];
 }
+
+#pragma mark - UICollectionViewDataSource,UICollectionViewDelegate
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 50;
+    
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ZGTestCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"testCell" forIndexPath:indexPath];
+    [cell.imgView sd_setImageWithURL:[NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1509269545368&di=220c095f54c46e1934a08942a8868037&imgtype=0&src=http%3A%2F%2Fwww.pp3.cn%2Fuploads%2F201507%2F2015072807.jpg"]];
+    
+    return cell;
+    
+}
+
 
 
 @end
